b854f442cef0e31f70dd41a8c270bcb2864940ec
Gonzalez Cortes <mp0644@arch-it05.arch.kit.edu>
2017-09-21 11:44:16 +0200

    local\git\updater

--- gitit.sh
+++ gitit.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
+# diese Script aktualisiert ziemlich einfach die lokale Verzeichniss von git
+# der Script aktualisiert alle geänderte Datei, wenn das ist nicht gewünscht dann
+# de "*" mit Dateiname ändert
+# eine Parameter ist pflichtig damit eine Beschreibung bei github steht.
+# diese Parameter ist für den Schalter -m 
+# Beispiel:
+# 	./gitit.sh File_counter
 git add *
 git commit -m $1
 git push
