401aa69e671403b788fd7d16d7cf98dd74190b6b
Carlos Gonzalez <xastherion@users.noreply.github.com>
2017-09-21 13:04:04 +0200

    Update gitit.sh

--- gitit.sh
+++ gitit.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# diese Script aktualisiert ziemlich einfach die lokale Verzeichniss von git
+# diese mini-Script aktualisiert ziemlich einfach die lokale Verzeichniss von git
 # der Script aktualisiert alle geänderte Datei, wenn das ist nicht gewünscht dann
 # de "*" mit Dateiname ändert
 # eine Parameter ist pflichtig damit eine Beschreibung bei github steht.
@@ -7,9 +7,9 @@
 # Beispiel:
 # 	./gitit.sh File_counter
 # ToDo:
-#   Spaces in messages
-# zum aktualisierung von gelöschte lokale Dateien ist eine message notwendig, wird aber nicht gesetz
-# also, etwas dummy nutzen.
+#   - Spaces in messages, bis her nur - _ / sind getestet. "messege is love" funktioniert nicht.
+#   - zum aktualisierung von gelöschte lokale Dateien ist eine message notwendig, wird aber nicht gesetz
+#     also, etwas dummy nutzen. Wird nicht gebraucht.
 
 git add *
 git commit -am $1
